cc9a5f958bb2f2399391cd511735247efec05e9c
allaura-dev <allaura.trainor@triosstudent.com>
2020-05-11 10:06:02 -0400

    complete
    only need the videos when able

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,7 @@
 
 import React from "react";
 import './AboutMe.css';
+import Me from './Allaura.png';
 
 function AboutMe() {
  return (
@@ -8,7 +9,8 @@ function AboutMe() {
    <div id="AboutDiv">
     <h2>About Me</h2>
     <hr />
-    <p>My name is Allaura Hanley. Tired of being a front line retail worker I decided to make a huge change in my life. I started with teaching myself about best code practices and Swift moving on to learn even more at triOS College in the Enterprise Web and Mobile Development course. Being creative and logic oriented, as others can attest I love picking apart plot holes in films and books, I have found my calling in code. Having done backend and frontend work I find that I enjoy both equally. I do my best to teach myself the new technologies and I am always open to learning the latest and greatest of the coder’s world.</p>
+    <div id="aboutPic"><img src={ Me } alt="A picture of Allaura" id="Me" /></div>
+    <div id="aboutText"><p>My name is Allaura Hanley. Tired of being a front line retail worker I decided to make a huge change in my life. I started with teaching myself about best code practices and Swift moving on to learn even more at triOS College in the Enterprise Web and Mobile Development course. Being creative and logic oriented, as others can attest I love picking apart plot holes in films and books, I have found my calling in code. Having done backend and frontend work I find that I enjoy both equally. I do my best to teach myself the new technologies and I am always open to learning the latest and greatest of the coder’s world.</p></div>
    </div>
  </div>
  );
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,7 +6,62 @@ function Resume() {
  return (
   
    <div id="resumeDiv">
-    <h1>Coming Soon!</h1>
+    <h1>OBJECTIVE</h1>
+    <p>Pursuing a Front End Web Developer Position at Northern Commerce, using my skills I have learned either on my own or through my 14 month course to help continue the success of the company.</p>
+    <h1>PROFESSIONAL SUMMARY</h1>
+    <ul class="Resume">
+      <li>-Detail oriented and timely on tasks.</li>
+      <li>-Able to make responsive web designs.</li>
+      <li>-Knowledge of multiple libraries available and their uses.</li>
+      <li>-Efficient at communication and planning with peers.</li>
+      <li>-Able to give and receive constructive criticism for the better.</li>
+      <li>-Created a personal project of an eCommerce site.</li>
+      <li>-Built a ASP.NET page using Bootstrap that uses an API.</li>
+    </ul>
+    <h1>SKILLS</h1>
+    <div id="Skills">
+    <ul class="Resume">
+     <div id="SkillsLeft">
+      <li>-HTML</li>
+      <li>-CSS</li>
+      <li>-Grunt</li>
+      <li>-React</li>
+      <li>-Bootstrap</li>
+      <li>-Razor ASP.net</li>
+      <li>-OOP</li>
+      <li>-JavaScript</li>
+     </div>
+      <div id="SkillsRight">
+       <li>-Java</li>
+       <li>-SQL</li>
+       <li>-C#</li>
+       <li>-C++</li>
+       <li>-git</li>
+       <li>-GitHub</li>
+       <li>-Illustrator</li>
+       <li>-Photoshop</li>
+       <li>-APIs</li>
+      </div>
+     </ul>
+    </div>
+    <h1>EDUCATION</h1>
+    <p>Enterprise Web and Mobile Development + Internship<br />
+         triOS College Business Technology Healthcare
+    </p>
+    <p class="ital">Currently attending</p>
+    <p class="ital">College Diploma</p>
+    <h1>VOLUNTEER</h1>
+    <p>PAL(Peer Assisted Learning) Tutor<br />
+        triOS College Business Technology Healthcare
+    </p>
+    <p class="ital">February 2020 – April 2021</p>
+    <p>Student Ambassador<br />
+     triOS College Business Technology Healthcare
+    </p>
+    <p class="ital">March 2020 – April 2021</p>
+    <p>Toronto Zoo</p>
+    <p class="ital">September 2019</p>
+    <p class="ital"><br />Please reach out to my on my LinkedIn for my email and phone contacts.<br /></p>
    </div>
 
  );
