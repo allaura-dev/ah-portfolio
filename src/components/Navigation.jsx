e0313e3f7e9a81e44e0a1b225307d97553471e97
allaura-dev <allaura.trainor@triosstudent.com>
2020-05-10 21:02:21 -0400

    Nav & Resume Set up
    Nav finished Resume only needs text input and socials set up

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,22 +2,29 @@
 import React from "react";
 import './Navigation.css';
 import { Link, withRouter } from "react-router-dom";
+import logo from './Logos/AHDevStudio.png';
+
 // imports logo???
 
 
 function Navigation() {
  return (
-  <div id="myNav">
-   <nav>
-    <ul id="navList">
-     <li id="leftNav">
-       <Link to="/portfolio" class="NavLinks">Portfolio</Link>
-     </li>
-     <li id="rightNav">
-       <Link to="/resume" class="NavLinks">Resume</Link>
-     </li>
-    </ul>      
-   </nav>
+  <div class="NavBarFull">
+   <div id="NavLogo">
+    <img src={ logo } alt="Main Logo" id="Logo" />
+   </div>
+   <div id="myNav">
+    <nav>
+     <ul id="navList">
+      <li id="leftNav">
+        <Link to="/portfolio" class="NavLinks">Portfolio</Link>
+      </li>
+      <li id="rightNav">
+        <Link to="/resume" class="NavLinks">Resume</Link>
+      </li>
+     </ul>      
+    </nav>
+   </div>
   </div>
  );
 }
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,13 +1,38 @@
 
 import React from "react";
 import './Socials.css';
+import CodePen from './SocialIcons/codepen-48.png';
+import GitHub from './SocialIcons/github-48.png';
+import Gmail from './SocialIcons/gmail-48.png';
+import LinkedIn from './SocialIcons/linkedin-48.png';
 
 function Socials() {
  return (
   <div id="socials">
-   <h4>The Tweeter</h4>
+   <div id="codepen">
+    <a href="https://codepen.io/allaura-dev">
+     <img src={ CodePen } alt="Main Logo" id="Logo" />
+    </a>
+   </div>
+   <div id="github">
+    <a href="https://github.com/allaura-dev">
+     <img src={ GitHub } alt="Main Logo" id="Logo" />
+    </a>
+   </div>
+   <div id="gmail">
+     <img src={ Gmail } alt="Main Logo" id="Logo" onclick="GMailPop()"/>
+   </div>
+   <div id="linkedin">
+    <a href="">
+     <img src={ LinkedIn } alt="Main Logo" id="Logo" />
+    </a>
+   </div>
   </div>
  );
+}
+
+function GMailPop() {
+    alert("alullaura")
 }
 
 export default Socials;
