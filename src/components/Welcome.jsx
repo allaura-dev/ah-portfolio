c9ff268f44d42a2a708464904503d4f4fb40d632
allaura-dev <allaura.trainor@triosstudent.com>
2020-05-11 10:09:49 -0400

    css fix

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -8,9 +8,10 @@ function Welcome() {
    <div id="WelcomeDiv">
     <h2>Welcome to AH! Dev Studio! </h2>
     <hr />
-    <p>This website will give you a view into some of my works with various technologies. The projects and works displayed  should give you a sense of my design philosophy and structure.
+    <p id="welcome">This website will give you a view into some of my works with various technologies. The projects and works displayed  should give you a sense of my design philosophy and structure.
         Enjoy travelling to code-finity and beyond!
     </p>
+    <br />
     <h3 id="catchLine">“Code so great it's scary!”</h3>
    </div>
   </div>
