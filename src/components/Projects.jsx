e8230c511fa403f58651366281adafaf55a4cb59
allaura-dev <allaura.trainor@triosstudent.com>
2020-05-11 02:47:44 -0400

    90% done
    just need internet to get the videos and my resume.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,9 +7,55 @@ import { Carousel } from 'react-responsive-carousel';
 class Projects extends React.Component {
  render() {
   return (
+   <div id="ProjectsDiv">
    <Carousel>
-
+    <div class="Project">
+     <div class="ProjectBlurb">
+      <h2 class="noHover">Escape From Orc Grotta</h2>
+      <p>In app game created using Java.</p>
+     </div>
+     <div class="ProjectVideos">
+      <h2>Video Here</h2>
+     </div>
+    </div>
+    <div class="Project">
+     <div class="ProjectBlurb">
+      <h2>Pet Walking Website</h2>
+      <p>A website with SQL database for storing customer and employee information as well as appointment management.</p>
+     </div>
+     <div class="ProjectVideos">
+      <h2>Video Coming Soon</h2>
+     </div>
+    </div>
+    <div class="Project">
+     <div class="ProjectBlurb">
+      <h2>Movie Maven</h2>
+      <p>A website designed using Razor and APIs to search actors, actress and movies. On load is shows movies in english with release dates in the next three months.</p>
+     </div>
+     <div class="ProjectVideos">
+      <h2>Video Coming Soon</h2>
+     </div>
+    </div>
+    <div class="Project">
+     <div class="ProjectBlurb">
+      <h2>Lord's Mobile Website</h2>
+      <p>A website created to help my guild with different aspects of the Lord's Mobile mobile phone game done in purely html and CSS.</p>
+     </div>
+     <div class="ProjectVideos">
+      <h2>Video Coming Soon</h2>
+     </div>
+    </div>
+    <div class="Project">
+     <div class="ProjectBlurb">
+      <h2>Lord's Mobile Website version 2</h2>
+      <p>An update to the previous site using Reactjs and an API created by myself in Firebase.</p>
+     </div>
+     <div class="ProjectVideos">
+      <h2>Video Coming Soon</h2>
+     </div>
+    </div>
    </Carousel>
+   </div>
   );
  }
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -11,7 +11,7 @@ function Welcome() {
     <p>This website will give you a view into some of my works with various technologies. The projects and works displayed  should give you a sense of my design philosophy and structure.
         Enjoy travelling to code-finity and beyond!
     </p>
-    <h3 id="catchLine">“Code so great is scary!”</h3>
+    <h3 id="catchLine">“Code so great it's scary!”</h3>
    </div>
   </div>
  );
