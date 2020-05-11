646305c569a60082b762c4b0a7fce1770419a6cb
allaura-dev <allaura.trainor@triosstudent.com>
2020-05-10 23:39:51 -0400

    about and welcome
    about and welcome 100% done

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,10 +4,13 @@ import './AboutMe.css';
 
 function AboutMe() {
  return (
-  <div id="AboutDiv">
-   <h2>About Me</h2>
-   <p>My name is Allaura Hanley. Tired of being a front line retail worker I decided to make a huge change in my life. I started with teaching myself about best code practices and Swift moving on to learn even more at triOS College in the Enterprise Web and Mobile Development course. Being creative and logic oriented, as others can attest I love picking apart plot holes in films and books, I have found my calling in code. Having done backend and frontend work I find that I enjoy both equally. I do my best to teach myself the new technologies and I am always open to learning the latest and greatest of the coder’s world.</p>
-  </div>
+  <div id="OutterAbout">
+   <div id="AboutDiv">
+    <h2>About Me</h2>
+    <hr />
+    <p>My name is Allaura Hanley. Tired of being a front line retail worker I decided to make a huge change in my life. I started with teaching myself about best code practices and Swift moving on to learn even more at triOS College in the Enterprise Web and Mobile Development course. Being creative and logic oriented, as others can attest I love picking apart plot holes in films and books, I have found my calling in code. Having done backend and frontend work I find that I enjoy both equally. I do my best to teach myself the new technologies and I am always open to learning the latest and greatest of the coder’s world.</p>
+   </div>
+ </div>
  );
 }
 
