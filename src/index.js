7a4fc1089860935ab9ab83a6d8dca3e60552e02e
allaura-dev <allauradev@gmail.com>
2020-08-06 17:18:50 -0400

    attmept 3

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import './index.css';
 import App from './App.js';
 
-ReactDOM.render( <App /> , <h1>Welcome to AH! Dev Studios please select the logo above to continue to my portfolio or select on of the other links given.</h1>,document.getElementById("root"));
+ReactDOM.render( <App /> , document.getElementById("root"));
 
 
 
