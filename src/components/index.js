65329677b0a5ffc093395b8312c8f70dec3e086f
allaura-dev <allauradev@gmail.com>
2020-08-06 17:19:58 -0400

    please work
    where does this code live if not here

--- src/components/index.js
+++ src/components/index.js
@@ -6,4 +6,4 @@ export { default as Portfolio} from "./Portfolio.jsx";
 
 export { default as Resume } from "./Resume.jsx";
 
-<h1>Welcome to AH! Dev Studios please select the logo above to continue to my portfolio or select on of the other links given.</h1>
+
