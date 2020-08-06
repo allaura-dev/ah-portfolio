92e158b997ada7b83d99f3197d3876f9032e28a2
allaura-dev <allauradev@gmail.com>
2020-08-06 17:16:35 -0400

    Change attempt 1

--- src/index.js
+++ src/index.js
@@ -6,3 +6,5 @@ import App from './App.js';
 
 ReactDOM.render( <App /> , document.getElementById("root"));
 
+<h1>Welcome to AH! Dev Studios please select the logo above to continue to my portfolio or select on of the other links given.</h1>
+
